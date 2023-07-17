4973b2371b8ec1efbe7ea22b630d8b126b43f54a
Manu <manuelitoys93@gmail.com>
2023-07-17 16:02:42 -0500

    Hooks Ordenados

--- useFetch/useFetch.js
+++ useFetch/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export const useFecth = ( url ) => {
+export const useFetch = ( url ) => {
 
     const [state, setState] = useState({
         data: null,
